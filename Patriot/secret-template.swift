3d473de64647a130bf579898588b2af7eccd8a82
Ron Lisle <ron@lisles.net>
2017-04-30 07:30:57 -0500

    Add secret file template

---
+++ Patriot/secret-template.swift
@@ -0,0 +1,27 @@
+//
+//  secret-template.swift
+//  Patriot
+//
+//  This file contains Particle.io login credentials.
+//  1. Enter your credentials
+//  2. Rename the file from secret-template to secret
+//  3. Add this file to the Patriot target
+//  4. Add this file to the IntegrationTests if adding a test device
+//
+//  Created by Ron Lisle on 4/8/17.
+//  Copyright © 2017 Ron Lisle. All rights reserved.
+//
+
+import Foundation
+
+
+struct Secret
+{
+    static let LoginEmail       = "<YourParticleLoginEmail>"
+    static let LoginPassword    = "<YourParticleLoginPW>"
+
+    //A separate test account for running integration tests
+    static let TestEmail        = "<YourTestParticleLogin>"
+    static let TestPassword     = "<YourTestParticlePW>"
+    static let TestDeviceId     = "<YourTestDeviceId>"
+}
