8d79260c09c153618e991967dcb1b250d0f1bdd0
Ron Lisle <ron@lisles.net>
2017-05-29 07:34:32 -0500

    Cleanup

--- Patriot/ConfigViewController.swift
+++ Patriot/ConfigViewController.swift
@@ -18,25 +18,4 @@ class ConfigViewController: UITableViewController {
     @IBAction func closeMenu(sender: AnyObject) {
         dismiss(animated: true, completion: nil)
     }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
